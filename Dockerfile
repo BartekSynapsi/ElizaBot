3e91ed50e6858fa3e376288e3fab3906fd5d21b0
Bartosz Karwacki <bartosz.karwacki@synapsi.xyz>
2021-03-19 22:23:08 +0100

    Docker gunicorn

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,4 @@ COPY . .
 RUN python3 -m pip install --upgrade pip &&\
     pip install -r requirements.txt
 
-EXPOSE 8000
-
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD gunicorn eliza.wsgi:application --bind 0.0.0.0:$PORT
--- eliza/settings.py
+++ eliza/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kz9m&euw+ehiv$igm$#sy--g)s3c)p4yc&j=^_=549oo1qsc8h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
