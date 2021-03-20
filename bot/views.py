01bb66e921fabdb135623df36cf84188479caca4
Bartosz Karwacki <bartosz.karwacki@synapsi.xyz>
2021-03-20 17:32:58 +0100

    Translation added

--- bot/views.py
+++ bot/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+from .eliza import el
 
 
-def home(request):
+def home(request):    
     return render(request, 'bot/home.html')
 
 @require_http_methods(['POST'])
 @csrf_exempt
 def bot(request):
     message = request.POST['message']
-    import time
-    return JsonResponse({"test": message})
+    if message in el.initials:
+        response = el.initial()
+    else:
+        response = el.respond(message)
+    
+    if response:
+        return JsonResponse({"reply": response})
+    return JsonResponse({"reply": el.final()})
